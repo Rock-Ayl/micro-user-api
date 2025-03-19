07f8b2e5d5a713571c9ad78dc22fb2893fe6fe9c
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-19 14:45:52 +0800

    修改：远程构建

---
+++ src/main/java/com/rock/micro/api/user/UserRemoteService.java
@@ -0,0 +1,26 @@
+package com.rock.micro.api.user;
+
+/**
+ * 用户服务远程接口
+ *
+ * @Author ayl
+ * @Date 2024-10-30
+ */
+public interface UserRemoteService {
+
+    /**
+     * 测试
+     *
+     * @return
+     */
+    String helloWorld();
+
+    /**
+     * 测试2
+     *
+     * @param word 测试单词
+     * @return
+     */
+    String helloWorld2(String word);
+
+}
